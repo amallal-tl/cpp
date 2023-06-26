81642fd31523c69b55b07f70b1113c5af03fe6e2
amallaltlai <amal.lal@outlook.com>
2023-06-26 21:18:54 +0530

    pointers

---
+++ pointers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int i = 7;
+	int* ptr = &i;
+	cout << *ptr << endl;
+	cout << ptr << endl;
+	++ptr;
+	cout << ptr << endl;
+	++ptr;
+	cout << ptr << endl;
+	ptr++;
+	cout << ptr << endl;
+	ptr++;
+	cout << ptr << endl;
+}
