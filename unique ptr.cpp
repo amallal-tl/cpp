1c7f65be55928895a40cef3c5cb995f51b69a41a
amallaltlai <amal.lal@outlook.com>
2023-07-03 21:16:22 +0530

    unique ptr

--- unique ptr.cpp
+++ unique ptr.cpp
@@ -1,35 +1,25 @@
 #include <iostream>
-using namespace std;
-// Dynamic Memory management library
 #include <memory>
 
-class Rectangle {
-	int length;
-	int breadth;
+using namespace std;
 
-public:
-	Rectangle(int l, int b)
-	{
-		length = l;
-		breadth = b;
+class P{
+	static int count;
+	public:
+	P(string message){cout << count << endl;}
+	~P(){cout << "Dest" << endl;}
+	void print(){
+		cout << "Printer loaded...." << count << endl;
 	}
-
-	int area() { return length * breadth; }
 };
 
-int main()
-{
-// --\/ Smart Pointer
-	unique_ptr<Rectangle> P1(new Rectangle(10, 5));
-	cout << P1->area() << endl; // This'll print 50
 
-	// unique_ptr<Rectangle> P2(P1);
-	unique_ptr<Rectangle> P2;
-	P2 = move(P1);
-
-	// This'll print 50
-	cout << P2->area() << endl;
-
-	// cout<<P1->area()<<endl;
-	return 0;
+int main(){
+	unique_ptr<P> p1(new P("Priner"));
+	p1->print();
+	unique_ptr<P> p2;
+	p2 = move(p1);
+	
+	p1->print();
+	p2->print();
 }
