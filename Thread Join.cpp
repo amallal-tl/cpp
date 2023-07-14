42047de96b81614c2381742a069249724443c311
amallaltlai <amal.lal@outlook.com>
2023-07-14 21:02:26 +0530

    Thread sleep and checking join sequence

--- Thread Join.cpp
+++ Thread Join.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 #include <thread>
+#include <unistd.h>
 
 using namespace std;
 
 void function1(){
-	cout << "Function1" << endl;
+    sleep(5);
 }
 
 void function2(){
-	cout << "Function2" << endl;
+    sleep(5);
 }
 
 int main(){
-	thread t1(function1);
-	thread t2(function2);
+	thread t1(function1);//Thread t1 created and execution initiated
+	thread t2(function2);//Thread t2 created and execution initiated
 	
-	t1.join();
-	t2.join();
+	//within 5 seconds both t1 and t2 will complete execution. Total time for both thread execution is 5 seconds
+	t1.join();//after t1 is joined, the system waits for 5 seconds sleep to complete and after completion it moves to next line.
+	cout << "Function1" << endl;
+	t2.join();//Since t2 is already executed along with t1, there is no wait and instanlty moves to next line.
+	cout << "Function2" << endl;
 	
 	cout << "All execution completed" << endl;
 }
